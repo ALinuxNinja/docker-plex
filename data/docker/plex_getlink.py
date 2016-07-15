833805d672b27fa8fba1380feafe6a23015c6d33
ALinuxNinja <github.dev@alinux.ninja>
2016-07-14 21:42:19 -0400

    It's time for python 3

--- data/docker/plex_getlink.py
+++ data/docker/plex_getlink.py
@@ -1,11 +1,11 @@
-#!/usr/bin/env python2
-import urllib2
+#!/usr/bin/env python3
+from urllib.request import urlopen
 import sys
 import json
 
-json_downloads = urllib2.urlopen('https://plex.tv/api/downloads/1.json')
-json_data = json.loads(json_downloads.read())
+json_downloads = urlopen('https://plex.tv/api/downloads/1.json').readall().decode('utf-8')
+json_data = json.loads(json_downloads)
 
 for distro in json_data["computer"]["Linux"]["releases"]:
         if distro["build"] == "linux-ubuntu-x86_64" and distro["distro"] == "ubuntu":
-                print distro["url"]
+                print (distro["url"])
